51cb4f7a1eca1178de909df21c7c06e38d0d11ad
Julek <kjuliusz87@gmail.com>
2024-05-12 17:10:33 +0200

    Dodanie bloków i html-i

--- Produkty/views.py
+++ Produkty/views.py
@@ -10,6 +10,13 @@ def index(request):
 
 def kategoria(request, id):
     kategoria_user = Kategoria.objects.get(pk=id)
+    kategorie_rowery = Produkty.objects.filter(kategoria=kategoria_user)
+    kategorie = Kategoria.objects.all()
+    dane = {'kategoria_user': kategoria_user,
+                 'kategorie_rowery': kategorie_rowery,
+                 'kategorie': kategorie}
+    return render(request, 'kategorie_rowery.html', dane)
+
 
 def produkt(request, id):
     produkt_user = Produkty.objects.get(pk=id)
